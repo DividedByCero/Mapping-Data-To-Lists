2e301bbf0e6687e7e9ca192bdbbbac1b92cd6f8d
DividedByCero <isaac4893@outlook.com>
2017-10-17 19:20:43 -0400

    create Benchmark proj for testing cases.

---
+++ DbReflectLib.Benchmark/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DbReflectLib;
+using DbReflectLib.Benchmark.Models;
+
+namespace DbReflectLib.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = "Data Source=.;Initial Catalog=Enterprise;Integrated Security=True";
+            SqlConnection Conn = new SqlConnection(ConnectionString);
+            TimeSpan executionTime = new TimeSpan();
+            
+            List<Supplier> Suppliers =  Mapper.Get<Supplier>(Conn, "SELECT * FROM Suppl", out executionTime);
+
+            Console.WriteLine(executionTime);
+            Console.ReadKey();
+        }
+    }
+}
