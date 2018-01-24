c98617b783f88bb7dad2846701d653e1f59733b5
Rafael I. Andujar <isaac4893@outlook.com>
2018-01-23 22:24:05 -0400

    include QueryConstructor Class

---
+++ SimpleDbToolkit/QueryConstructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDbToolkit
+{
+    public class QueryConstructor<T>
+    {
+        public static List<T> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
