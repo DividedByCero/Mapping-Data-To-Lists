9c42225fc9b6286f64d6f54bf109e01b010978b8
Rafael I. Andujar <isaac4893@outlook.com>
2018-01-23 21:04:48 -0400

    add first incomplete test case

--- SimpleDbToolkit.Tests/MapperTestCase.cs
+++ SimpleDbToolkit.Tests/MapperTestCase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Moq;
+using System.Data;
 namespace SimpleDbToolkit.Tests
 {
     [TestClass]
@@ -9,6 +10,15 @@ namespace SimpleDbToolkit.Tests
         [TestMethod]
         public void DidTheLogicFlowGoesWell()
         {
+            var connection = new Mock<IDbConnection>();
+            var reader = new Mock<IDataReader>();
+            var command = new Mock<IDbCommand>();
+
+            connection.Setup(x => x.CreateCommand()).Returns(() => command.Object);
+            command.Setup(x => x.ExecuteReader()).Returns(() => reader.Object);
+
+
+            //...
 
         }
     }
