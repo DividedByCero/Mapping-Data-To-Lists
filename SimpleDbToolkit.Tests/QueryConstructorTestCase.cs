a7c2f27a22582bbfeaab42178fe1e1991387e67a
DividedByCero <isaac4893@outlook.com>
2018-03-16 09:17:40 -0400

    create QueryConstructor<T>.SelectAll TestCase

---
+++ SimpleDbToolkit.Tests/QueryConstructorTestCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleDbToolkit.Tests
+{
+    [TestClass]
+    public class QueryConstructorTestCase
+    {
+        [TestMethod]
+        public void DoesSelectAllReturnCorrectly()
+        {
+            var query = QueryConstructor<MockObjects.MockItem>.SelectAll();
+
+            Assert.AreEqual(query, "SELECT Id, Name, FROM MockItem");
+
+        }
+    }
+}
