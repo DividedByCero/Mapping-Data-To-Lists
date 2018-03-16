6c008ea662c65ef3de53c7a097b428a37c3ba423
DividedByCero <isaac4893@outlook.com>
2018-03-16 09:17:00 -0400

    Implement QueryConstructor<T>.SelectAll

--- SimpleDbToolkit/QueryConstructor.cs
+++ SimpleDbToolkit/QueryConstructor.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Reflection;
 namespace SimpleDbToolkit
 {
-    public class QueryConstructor<T>
+    public static class QueryConstructor<T> where T : class, new()
     {
-        public static List<T> SelectAll()
+        public static string SelectAll() 
         {
-            throw new NotImplementedException();
+            Type sample = new T().GetType();
+            PropertyInfo[] props = sample.GetProperties();
+            var statement = new string[props.Count() + 3];
+            statement[0] = "SELECT";
+            int x = 1;
+
+            foreach(var prop in props)
+            {
+                statement[x] = prop.Name + ",";
+                x++;
+            }
+
+            statement[x++] = "FROM";
+            statement[x++] = sample.Name;
+
+            return string.Join(" ", statement);
         }
     }
 }
