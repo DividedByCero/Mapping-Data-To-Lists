66cc5b61889be1ebf504fe7f05d4f27b7ca7d06b
DividedByCero <isaac4893@outlook.com>
2018-04-23 21:38:01 -0400

    Add Insert Generator Function.

---
+++ SimpleDbToolkit.Tests/MockObjects/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleDbToolkit.Annotations;
+namespace SimpleDbToolkit.Tests.MockObjects
+{
+    [TableName("CUSTOMERS")]
+    public class Customer
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CreditNumber { get; set; }
+        public long Identification { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ SimpleDbToolkit.Tests/QueryConstructorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleDbToolkit;
+using SimpleDbToolkit.Annotations;
+using SimpleDbToolkit.Tests.MockObjects;
+
+namespace SimpleDbToolkit.Tests
+{
+    [TestClass]
+    public class QueryConstructorTest
+    {
+        [TestMethod]
+        public void DoesGenerateAInsertStatement()
+        {
+            Customer customer = new Customer();
+            customer.CreditNumber = 1231231;
+            customer.Id = 42;
+            customer.LastName = "Foo";
+            customer.Name = "Bar";
+
+            var constructor = new QueryConstructor<Customer>();
+            string result = constructor.Insert(customer);
+        }
+    }
+}
