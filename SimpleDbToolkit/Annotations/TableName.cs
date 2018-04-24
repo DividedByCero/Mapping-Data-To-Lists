a3d6d386f3b0c943b470014d2e48b353d220661b
DividedByCero <isaac4893@outlook.com>
2018-04-23 21:37:43 -0400

    add AutoIncrement and TableName custom Annotations.

---
+++ SimpleDbToolkit/Annotations/TableName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDbToolkit.Annotations
+{
+    public class TableName : Attribute
+    {
+        public string Name { get; set; }
+
+        public TableName(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- SimpleDbToolkit/QueryConstructor.cs
+++ SimpleDbToolkit/QueryConstructor.cs
@@ -3,14 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SimpleDbToolkit.Annotations;
 namespace SimpleDbToolkit
 {
     public class QueryConstructor<T>
     {
-        public static List<T> SelectAll()
+        private string ResolveColumnValue(object value)
         {
-            throw new NotImplementedException();
+            if (value == null)
+                return "null";
+
+            if (value.GetType() == typeof(string))
+            {
+                return "'" + value.ToString() + "'";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+
+        private bool validateCustomAttribute<Y>(System.Reflection.PropertyInfo prop) where Y : Attribute
+        {
+            return prop.GetCustomAttributes(typeof(Y), false).Count() == 0;
+        }
+
+        public string Insert(T item)
+        {
+            var type = item.GetType();
+            var properties = type.GetProperties().Where(prop => {
+                return validateCustomAttribute<AutoIncrement>(prop);
+            });
+
+            StringBuilder builder = new StringBuilder("INSERT INTO ");
+            object TableNameAttribute = type.GetCustomAttributes(typeof(TableName), false).FirstOrDefault();
+            string tablename = TableNameAttribute == null ? type.Name : ((TableName)TableNameAttribute).Name;
+
+            builder.Append(string.Format("{0}({1})", tablename, String.Join(", ", properties.Select(prop => prop.Name))));
+            IEnumerable <string> values = properties.Select(x => {
+                return ResolveColumnValue(x.GetValue(item));
+            });
+
+            builder.Append(string.Format(" VALUES({0});", string.Join(", ", values.ToArray())));
+            return builder.ToString();
         }
     }
 }
